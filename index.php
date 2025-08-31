ad1e352a21f4d7c628e2d0996aee8e720b7d15af
GabrielN-Dev <gabrielnogueira0777@gmail.com>
2025-08-31 20:33:19 -0300

    teste_2

--- index.php
+++ index.php
@@ -1,4 +1,19 @@
-<?php
-echo "teste </br>";
-echo" teste2";
-?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+
+    <?php
+    echo "teste </br>";
+    echo " teste2";
+    ?>
+</body>
+
+</html>
